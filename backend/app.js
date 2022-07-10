4c964dd923a9d2e7023084b952b10233daa26e94
Evgeny Ovodkov <kefir4ek@gmail.com>
2022-07-11 02:00:41 +0300

    Fix

--- backend/app.js
+++ backend/app.js
@@ -23,8 +23,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 });
 
 const allowedCors = [
-  'https://project.mesto.nomorepartiesxyz.ru',
-  'http://project.mesto.nomorepartiesxyz.ru',
+  'https://project.mesto.nomorepartiesxyz.ru/',
+  'http://project.mesto.nomorepartiesxyz.ru/',
   'localhost:3000',
   'http://localhost:3000',
 ];
