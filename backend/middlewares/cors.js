7ac25d37db62def5dffe30cec81b53403b5f2cf2
Evgeny Ovodkov <kefir4ek@gmail.com>
2022-07-11 18:56:21 +0300

    Fix

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -20,7 +20,7 @@ module.exports = (req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.status(200).send();
+    return res.status(200).end();
   }
 
   next();
