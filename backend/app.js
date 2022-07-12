1f57635561b4b63c1f20ca1b5f0fd3223fe70f28
Evgeny Ovodkov <kefir4ek@gmail.com>
2022-07-13 00:48:15 +0300

    Fix Joi

--- backend/app.js
+++ backend/app.js
@@ -47,14 +47,14 @@ app.get('/crash-test', () => {
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    // password: Joi.string().required(),
+    password: Joi.string().required(),
   }),
 }), login);
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    // password: Joi.string().required(),
+    password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().custom(linkValidation),
